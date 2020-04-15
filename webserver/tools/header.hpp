2844bdac86916d4644bbcd0bd909b142aa7aebc8
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-04-15 19:16:29 +0300

    Add template of request

---
+++ webserver/tools/header.hpp
@@ -0,0 +1,9 @@
+#ifndef WEBSERVER_HEADER_HPP
+#define WEBSERVER_HEADER_HPP
+
+struct Header {
+    std::string name;
+    std::string value;
+};
+
+#endif // WEBSERVER_HEADER_HPP
---
+++ webserver/tools/task.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBSERVER_TASK_HPP
+#define WEBSERVER_TASK_HPP
+
+#include <string>
+
+struct Task {
+    std::string* query;
+    // ...
+};
+
+#endif // WEBSERVER_TASK_HPP
