ace4a9ef9f4350969bdd61bbfcccadb805a9180e
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-05-10 01:16:35 +0300

    Add http server class

---
+++ webserver/http_server/http_server.cpp
@@ -0,0 +1,34 @@
+#include <thread>
+#include <vector>
+
+#include "http_server.hpp"
+
+namespace net = boost::asio;
+using Listener = pasteslash::webserver::listener::Listener;
+
+namespace pasteslash {
+namespace webserver {
+namespace server {
+
+HttpServer::HttpServer(std::string address, std::string port, int threads) :
+threads_(threads),
+ioc(net::io_context(threads)) {
+    auto const adrs = net::ip::make_address(address);
+    const unsigned short prt = static_cast<unsigned short>(std::atoi(port.c_str()));
+
+    // Create and launch a listening port
+    std::make_shared<Listener>(ioc, tcp::endpoint{adrs, prt})->run();
+}
+
+void HttpServer::run() {
+    std::vector<std::thread> v;
+    v.reserve(threads_ - 1);
+    for(auto i = threads_ - 1; i > 0; --i) {
+        v.emplace_back([this] { ioc.run(); });
+    }
+    ioc.run();
+}
+
+} // server
+} // webserver
+} // pasteslash
---
+++ webserver/http_server/http_server.hpp
@@ -0,0 +1,34 @@
+#ifndef WEBSERVER_HTTP_SERVER_HPP
+#define WEBSERVER_HTTP_SERVER_HPP
+
+#include <boost/beast/core.hpp>
+#include <boost/noncopyable.hpp>
+#include <memory>
+#include <string>
+
+#include "../listener/listener.hpp"
+
+namespace net = boost::asio;
+
+namespace pasteslash {
+namespace webserver {
+namespace server {
+
+// HTTP server
+class HttpServer : boost::noncopyable {
+public:
+    HttpServer(std::string address, std::string port, int threads = 1);
+
+    // Start running server
+    void run();
+
+private:
+    unsigned short threads_;
+    net::io_context ioc;
+};
+
+} // server
+} // webserver
+} // pasteslash
+
+#endif // WEBSERVER_HTTP_SERVER_HPP
