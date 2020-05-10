d82ea688c1a4a5043eeeedb4755e21ab2b65365e
OlegGibadulin <O.Gibadulin@yandex.ru>
2020-05-10 16:05:45 +0300

    Add error display function

---
+++ webserver/utils/output.cpp
@@ -0,0 +1,13 @@
+#include "output.hpp"
+
+namespace pasteslash {
+namespace webserver {
+namespace utils {
+
+void fail(beast::error_code ec, char const* what) {
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+} // utils
+} // webserver
+} // pasteslash
---
+++ webserver/utils/output.hpp
@@ -0,0 +1,19 @@
+#ifndef WEBSERVER_OUTPUT_HPP
+#define WEBSERVER_OUTPUT_HPP
+
+#include <boost/beast/core.hpp>
+#include <iostream>
+
+namespace beast = boost::beast;
+
+namespace pasteslash {
+namespace webserver {
+namespace utils {
+
+void fail(beast::error_code ec, char const* what);
+
+} // utils
+} // webserver
+} // pasteslash
+
+#endif // WEBSERVER_OUTPUT_HPP
