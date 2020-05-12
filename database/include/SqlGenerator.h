f0bb0f1f4aed226e23e834185ccdec54a2894d3d
Oleg <moxxxie07@gmail.com>
2020-05-12 16:53:46 +0300

    add SQLGenerator implementation

--- database/include/SqlGenerator.h
+++ database/include/SqlGenerator.h
@@ -2,16 +2,32 @@
 #define DATABASE_SQLGENERATOR_H
 
 #include "string"
-#include "DatabaseObject.h"
-#include "unordered_set"
 #include "dataFormat.h"
-#include "Meta.h"
 
 class SqlGenerator {
 public:
-    std::string generateAddQuery(dataFormat, Meta);
-    std::string generateDeleteQuery(dataFormat , Meta);
-    std::string generateGetQuery(dataFormat, Meta);
+    // fieldValueMap: map like {field_1: value_1, field_2: value_2}
+    // Generate query like
+    // INSERT INTO intoTableName(field_1, field_2 ...) VALUES (value_1, value_2...)
+    static std::string generateAddQuery(const std::string& into,
+            const dataFormat& fieldValueMap);
+
+    // Generate query like
+    // UPDATE tableName SET field_1 = newValue1, field_2 = newValue2 ...
+    // WHERE conditionField_n = conditionValue_n ...
+    static std::string generateUpdateQuery(const std::string& tableName,
+            const dataFormat& conditionMap, const dataFormat& newValuesMap,
+            const std::string& sign="=");
+
+    // Generate query like
+    // DELETE FROM tableName WHERE conditionField_1=conditionValue_1...
+    static std::string generateDeleteQuery(const std::string& tableName,
+            const dataFormat& conditionMap, const std::string& sign="=");
+
+    // Generate query like
+    // SELECT * FROM fromTableName WHERE conditionField_1=conditionValue_1...
+    static std::string generateGetQuery(const std::string& from,
+           const dataFormat& conditionMap, const std::string& fields="*");
 };
 
 #endif // DATABASE_SQLGENERATOR_H
--- database/src/SqlGenerator.cpp
+++ database/src/SqlGenerator.cpp
@@ -1,1 +1,71 @@
-#include "SqlGenerator.h"
+#include "../include/SqlGenerator.h"
+
+std::string stringWithQuotes(const std::string& string) {
+    return "'" + string + "'";
+}
+
+std::string SqlGenerator::generateAddQuery(const std::string& into,
+        const dataFormat& fieldValueMap) {
+    std::string sqlQuery = "INSERT INTO " + into + "(";
+
+    for (auto & field : fieldValueMap)
+        sqlQuery += field.first + ", ";
+
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+    sqlQuery += ") VALUES (";
+
+    for (auto &value : fieldValueMap)
+        sqlQuery += stringWithQuotes(value.second) + ", ";
+
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+    sqlQuery += ");";
+
+    return sqlQuery;
+}
+
+std::string SqlGenerator::generateGetQuery(const std::string& from,
+        const dataFormat& conditionMap, const std::string& fields) {
+    std::string sqlQuery = "SELECT " + fields + " FROM " + from +
+                           " WHERE ";
+
+    for (const auto& i: conditionMap)
+        sqlQuery += i.first + "=" + stringWithQuotes(i.second) + ", ";
+
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+    sqlQuery += ";";
+
+    return sqlQuery;
+}
+
+std::string SqlGenerator::generateDeleteQuery(const std::string& tableName,
+        const dataFormat &conditionMap, const std::string& sign) {
+    std::string sqlQuery = "DELETE FROM " + tableName +
+                           " WHERE ";
+
+    for (const auto& i: conditionMap)
+        sqlQuery += i.first + sign + stringWithQuotes(i.second) + ", ";
+
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+    sqlQuery += ";";
+
+    return sqlQuery;
+}
+
+std::string SqlGenerator::generateUpdateQuery(const std::string& tableName,
+        const dataFormat& conditionMap, const dataFormat& newValuesMap,
+        const std::string& sign) {
+    std::string sqlQuery = "UPDATE " + tableName + " SET ";
+
+    for (const auto& i: newValuesMap)
+        sqlQuery += i.first + sign + stringWithQuotes(i.second) + ", ";
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+
+    sqlQuery += " WHERE ";
+    for (const auto& i: conditionMap)
+        sqlQuery += i.first + sign + stringWithQuotes(i.second) + ", ";
+
+    sqlQuery = sqlQuery.substr(0, sqlQuery.size() - 2);
+    sqlQuery += ";";
+
+    return sqlQuery;
+}
